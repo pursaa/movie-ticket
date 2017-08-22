4f490cbdb22f4cfb80f36c9c200b01d26f507455
Debbie Michel and Luciano Oliveira <debbie@>
2017-08-22 15:15:00 -0700

    This will console.log the price of the movie per age group

--- js/scripts.js
+++ js/scripts.js
@@ -6,32 +6,34 @@ function Ticket(movie, time, age) {
 }
   Ticket.prototype.price = function(){
     var total = this.movie + this.time + this.age;
-    var result;
-    if(total <= 18 && total > 15){
-      console.log("The price 5");
-      result = 'The price is 5';
+        // var result;
 
-    } else if(total > 18) {
-      console.log("the price of the movie is $6");
-
-    }  else {
-      console.log("you can't watch this movie");
+    if(this.age <= 15 && this.time <=2 && this.movie === 4){
+      console.log("No Way!!");
+    }else if(this.age <= 15 && this.time === 1 && this.movie < 4){
+      console.log("Price will be $4.00");
+    }else if(this.age <= 15 && this.time === 2 && this.movie < 4){
+      console.log("Price will be $5.00");
+    }else if(this.age > 64 && this.time <= 2 && this.movie <= 4){
+      console.log("Price will be $4.50");
+    }else if(this.age > 15 && this.age <65 && this.time === 1 && this.movie <= 4){
+      console.log("Price will be $6.00");
+    }else{
+      console.log("Price will be $7.00");
     }
-    return result;
-  }
-
+    return total;
+}
 //User Inerface Logic
 $(document).ready(function(){
   $("form.tickets").submit(function(event){
-
     event.preventDefault();
     var movie = $('#movies').val();
     var time = $('#time').val();
     var age = $('input#age').val();
-    // var output = $('.output').text(myMovie.price());
+
     //var movie = $('#movies option:selected').attr('value');
-    var myMovie = new Ticket(parseInt(movie), parseInt(time), parseInt(age));
-    // console.log(myMovie);
+      var myMovie = new Ticket(parseInt(movie), parseInt(time), parseInt(age));    // console.log(myMovie);
     console.log('This is the result of my obj:', myMovie.price());
+    
   });
 });
